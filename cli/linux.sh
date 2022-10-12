10e52733e9983fbe704c9ffff053a8e0e1e46c90
aunknown@333 <96411763+aunknownwhoami@users.noreply.github.com>
2022-10-12 20:54:24 +0530

    Update linux.sh

--- cli/linux.sh
+++ cli/linux.sh
@@ -4,17 +4,25 @@ set -e
 #script requires sudo permissions
 if [ $EUID != 0 ]; then
     echo "Please run as root"
+    printf "\e[1;31mPlease install sudo curl git wget \e[0m"
     exit 1
+
+  else
+    echo -e "\e[1;31mRunning as root\1[0m\n"
 fi
 
-#update system 
-printf '\e[1;33m[Note]: \e[0m\e[1;32mUpdating system \e[0m\n'
-sudo apt update 2> output.txt  && sudo apt upgrade -y 2> output.txt
-#install depancies 
-printf '\e[1;33m[Note]: \e[0m\e[1;32mInstalling depancies \e[0m\n'
-sudo apt install -y curl wget git sudo 2> output.txt
+#installing dependencies
+if ! command -v  curl git wget &> /dev/null
+then
+    echo "Installing dependencies"
+    sudo apt  update
+    sudo apt  install curl git wget -y
+fi
 
-rm output.txt
+#space for future dependencies
+function space {
+  echo -e "\n"
+}
 
 clear
 
@@ -27,45 +35,47 @@ if [ -f /etc/apt/sources.list ]; then
 fi
 
 #installing tools user choice number of tools installation 
-read -p "Enter the number of tools you want to install[eg: 1 3 5 6]: " -a  install_tools
+read -p $'\n\e[1;34mEnter the number of tools you want to install\e[0m[eg: 1 3 5 6]: ' -a  install_tools
 
 for i in "${install_tools[@]}"
 do
     case $i in
         1)
-            printf "\e[1;33m[Note]: \e[0m\e[1;32mInstalling lsd \e[0m\n"
+            printf "\e[1;33m[Note]: \e[0m\e[1;31mInstalling lsd \e[0m\n"
             wget https://github.com/Peltoche/lsd/releases/download/0.23.1/lsd_0.23.1_amd64.deb && sudo dpkg -i lsd_0.23.1_amd64.deb  
             rm -rf lsd_0.23.1_amd64.deb
+            space
             ;;
-        2)while read -r "array[$i]"; do
-    ((i++))
-done
-
-
-            printf "\e[1;33m[Note]: \e[0m\e[1;32mInstalling bat \e[0m\n"
+        2)
+            printf "\e[1;33m[Note]: \e[0m\e[1;31mInstalling bat \e[0m\n"
             wget https://github.com/sharkdp/bat/releases/download/v0.22.1/bat_0.22.1_amd64.deb && sudo dpkg -i bat_0.22.1_amd64.deb
             rm -rf bat_0.22.1_amd64.deb
+            space
             ;;
         3)
-            printf "\e[1;33m[Note]: \e[0m\e[1;32mInstalling fd \e[0m\n"
+            printf "\e[1;33m[Note]: \e[0m\e[1;31mInstalling fd \e[0m\n"
             wget https://github.com/sharkdp/fd/releases/download/v8.4.0/fd_8.4.0_amd64.deb && sudo dpkg -i fd_8.4.0_amd64.deb
             rm -rf fd_8.4.0_amd64.deb
+            space
             ;;
         4)
-            printf "\e[1;33m[Note]: \e[0m\e[1;32mInstalling ripgrep \e[0m\n"
+            printf "\e[1;33m[Note]: \e[0m\e[1;31mInstalling ripgrep \e[0m\n"
             wget https://github.com/BurntSushi/ripgrep/releases/download/13.0.0/ripgrep_13.0.0_amd64.deb && sudo dpkg -i ripgrep_13.0.0_amd64.deb 
             rm -rf ripgrep_13.0.0_amd64.deb
+            space
             ;;
         5)
-            printf "\e[1;33m[Note]: \e[0m\e[1;32mInstalling fzf \e[0m\n"
+            printf "\e[1;33m[Note]: \e[0m\e[1;31mInstalling fzf \e[0m\n"
             sudo apt install fzf
+            space
             ;;
         6)
-            printf "\e[1;33m[Note]: \e[0m\e[1;32mInstalling jq \e[0m\n"
+            printf "\e[1;33m[Note]: \e[0m\e[1;31mInstalling jq \e[0m\n"
             sudo apt install -y jq
+            space
             ;;
         7)
-            printf "\e[1;33m[Note]: \e[0m\e[1;32mInstalling sd \e[0m\n"
+            printf "\e[1;33m[Note]: \e[0m\e[1;31mInstalling sd \e[0m\n"
             read -p "Depanding on cargo packagemanger .Do yout want to install [y/n] " sd
             case $sd in
               [yY][eE][sS]|[yY])
@@ -73,6 +83,7 @@ done
                 cargo install sd
                 echo "export PATH=$PATH:$HOME/.cargo/bin" >> ~/.bashrc && echo "export PATH=$PATH:$HOME/.cargo/bin" >> ~/.zshrc
                 echo "Relaod your terminal"
+                space
                 ;;
             [nN][oO]|[nN])
                     printf "\e[1;33m[Note]: \e[0m\e[1;32mSkipping sd \e[0m\n"
@@ -81,30 +92,38 @@ done
 
             ;;
         8)
-            printf "\e[1;33m[Note]: \e[0m\e[1;32mInstalling cheat \e[0m\n"
+            printf "\e[1;33m[Note]: \e[0m\e[1;31mInstalling cheat \e[0m\n"
             sudo apt install gzip
             wget https://github.com/cheat/cheat/releases/download/4.3.3/cheat-linux-amd64.gz && gunzip cheat-linux-amd64.gz  
             sudo mv cheat-linux-amd64 /usr/share/bin/cheat
             sudo chmod +x /usr/share/bin/cheat
+            space
             ;;
         9)
-            printf "\e[1;33m[Note]: \e[0m\e[1;32mInstalling tldr \e[0m\n"
+            printf "\e[1;33m[Note]: \e[0m\e[1;31mInstalling tldr \e[0m\n"
             sudo apt install nodejs npm 
              sudo npm install -g tldr || npm install -g tldr
+            space
             ;;
         10)
-            printf "\e[1;33m[Note]: \e[0m\e[1;32mInstalling zoxide \e[0m\n"
+            printf "\e[1;33m[Note]: \e[0m\e[1;31mInstalling zoxide \e[0m\n"
             wget https://github.com/ajeetdsouza/zoxide/releases/download/v0.8.3/zoxide_0.8.3_amd64.deb && sudo dpkg -i zoxide_0.8.3_amd64.deb
             rm -rf zoxide_0.8.3_amd64.deb
             echo '\e[1;33m[Note]: \e[0m\e[1;32mAdd this line to your .bashrc or .zshrc file [eval $(zoxide init (bash/zsh))] \e[0m\n'
             echo 'eval "$(zoxide init bash)"' >> ~/.bashrc && echo 'eval "$(zoxide init zsh)"' >> ~/.zshrc
             source ~/.bashrc || source ~/.zshrc
+            space
             ;;
         *)
            printf "\e[1;31m[Error]: \e[0m\e[1;32mInvalid option \e[0m\n"
             ;;
     esac
 done
+
+#repor for any issues on github page 
+echo -e "\e[1;33m[Note]: \e[0m\e[1;32mIf you have any issues please report on github page \e[0m\n"
+echo -e "\e[1;33m[Note]: \e[0m\e[1;32mhttps://github.com/aunknownwhoami \e[0m\n"
+
 
 
 
